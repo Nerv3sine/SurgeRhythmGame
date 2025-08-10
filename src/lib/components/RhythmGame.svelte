a33fba49172c9c59a2dd44548a44fa3848308e38
Jason Cai <jasoncai64@gmail.com>
2025-08-09 23:26:51 -0700

    reorganized the project structure

---
+++ src/lib/components/RhythmGame.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import Background from "./Background.svelte";
+</script>
+
+<div id="rhythmGame">
+    <Background/>
+</div>
+
+<style>
+    *{
+        --rhythmViewportHeight: 80vh;
+    }
+
+    #rhythmGame{
+        height: var(--rhythmViewportHeight);
+        border: 1px solid;
+        width: calc(100vw  - 2px);
+        
+        overflow-x: hidden;
+        background: #AADCFF;
+    }
+</style>
